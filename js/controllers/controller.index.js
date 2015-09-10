f59981e7fd64586dd38b889e277efe2e11b886c8
yc杨晨 <yangchen@idianjia.cn>
2015-09-10 09:54:47 +0800

    .....

--- js/controllers/controller.index.js
+++ js/controllers/controller.index.js
@@ -8,6 +8,6 @@ var iCloudController = angular.module("iCloudController", ["ngCookies"]);
 
 iCloudController.controller("IndexController", ["$scope", "$http", "$window",
     function ($scope, $http, $window) {
-        $window.alert($window.host);
+
     }]);
 

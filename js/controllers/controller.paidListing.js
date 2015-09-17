b06d643675e8862ecc04d245cf2b5abbfb0148e8
canyun0228 <canyun@live.com>
2015-09-17 19:26:55 +0800

    service->$grid

--- js/controllers/controller.paidListing.js
+++ js/controllers/controller.paidListing.js
@@ -2,19 +2,8 @@
  * Created by chen on 2015/9/15.
  */
 
-iCloudController.controller("PaidlistingController", ["$scope", "$http", "$cookieStore", "$window", "$icloudGrid",
-    function ($scope, $http, $cookieStore, $window, $icloudGrid) {
-        var options = {
-            gridOptions: {
-                customFilter: {
-                    id: "id__icontains",
-                    bid_price: "bid_price__icontains"
-                }
-            }
-        };
-
-        var grid = $icloudGrid.initial($window.paidlisting_url, options);
-        grid.setEnableSelect(true);
-        $scope.paidlistingGrid = grid.gridOptions;
-
+iCloudController.controller("PaidlistingController", ["$scope", "$http", "$cookieStore", "$window", "$grid", "$checkBox",
+    function ($scope, $http, $cookieStore, $window, $grid, $checkBox) {
+        $grid.initial($scope, $window.paidlisting_url);
+        $checkBox.enableCheck("table-paidlisting");
     }]);

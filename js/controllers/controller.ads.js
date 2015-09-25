3df04d39c96ee92551436d928d25da734b35eaee
canyun0228 <canyun@live.com>
2015-09-25 18:31:06 +0800

    permissions

--- js/controllers/controller.ads.js
+++ js/controllers/controller.ads.js
@@ -5,21 +5,21 @@ iCloudController.controller("AdsController", ["$scope", "$http", "$cookieStore",
     function ($scope, $http, $cookieStore, $window, $grid, $checkBox) {
         var grid = $grid.initial($scope, $window.wemedia_url);
         $checkBox.enableCheck("table-wemedia");
-        $scope.send =function(){
+        $scope.send = function () {
             $scope.$emit('sendData');
         }
     }]);
 
 
-iCloudController.controller("WeMediaEditController", ["$scope", "$http", "$cookieStore", "$window", "$uploadImg","$category",
-    function ($scope, $http, $cookieStore, $window, $uploadImg,$category) {
-        $scope.$on('executeData',function(){
+iCloudController.controller("WeMediaEditController", ["$scope", "$http", "$cookieStore", "$window", "$uploadImg", "$category",
+    function ($scope, $http, $cookieStore, $window, $uploadImg, $category) {
+        $scope.$on('executeData', function () {
             console.log($scope.p.title);
-        })
+        });
         var promise = $category.get();
-        promise.success(function(data){
-            $scope.ad_category=data.results;
-        })
+        promise.success(function (data) {
+            $scope.ad_category = data.results;
+        });
         $scope.uploadImg = function () {
             if ($scope.ad_title == null || $scope.ad_url == null || $scope.selectId == null || angular.element("#avatarInput").val() == null) {
                 alert("信息填写不完整")

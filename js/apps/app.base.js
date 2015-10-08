faa1f1cd50120b74cd635f868b6e85bfa770a291
yc杨晨 <yangchen@idianjia.cn>
2015-10-08 19:30:51 +0800

    设备管理界面1

--- js/apps/app.base.js
+++ js/apps/app.base.js
@@ -58,4 +58,19 @@ iCloudApp.config(function ($stateProvider, $urlRouterProvider) {
             templateUrl:"template/center/shop/shop_management.html",
             controller:"ShopManagementController"
         })
+        .state("main.version_manage",{
+            url:"/version_management",
+            templateUrl:"template/center/rooter/version_manage.html",
+            controller:"VersionManagementController"
+        })
+        .state("main.firmware_update",{
+            url:"/firmware_update",
+            templateUrl:"template/center/rooter/firmware_update.html",
+            controller:"FirmwareUpdateController"
+        })
+        .state("main.details",{
+            url:"/details",
+            templateUrl:"template/center/rooter/details.html",
+            controller:"DetailsController"
+        })
 });
--- js/controllers/controller.eq.js
+++ js/controllers/controller.eq.js
@@ -9,3 +9,19 @@ iCloudController.controller("EqManagementController", ['$scope', function ($scop
         }
     }
 }])
+
+iCloudController.controller("VersionManagementController", ['$scope', function ($scope) {
+
+}])
+
+iCloudController.controller("FirmwareUpdateController", ['$scope', function ($scope) {
+   $scope.update =function(){
+       if(confirm("升级过程中路由器将会停止运行，升级完成后路由器将自动重启。请确认是否升级所选路由器固件版本？")){
+
+       }
+   }
+}])
+
+iCloudController.controller("DetailsController", ['$scope', function ($scope) {
+
+}])

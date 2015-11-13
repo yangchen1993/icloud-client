1f9fdbb9d1d3db2c1ed24a9b35cb8468b975575b
yc杨晨 <yangchen@idianjia.cn>
2015-11-13 19:14:15 +0800

    no can no BB

--- js/controllers/controller.eq.js
+++ js/controllers/controller.eq.js
@@ -165,10 +165,136 @@ iCloudController.controller("DetailsController", ['$scope', '$http', '$cookieSto
             }
         }, 1000)
     };
+
+    //放行设置
+    var reload_blackwihit = function(){
+        $scope.type = ["MAC", "域名"];
+        $http.get([window.API.ROUTER.GET_ROUTER_BLACK_WHITES, "?key=", $cookieStore.get("key"), "&router=", router_id].join("")).success(function (data) {
+            $scope.policy = data.results;
+            for (var i = 0; i < data.results.length; i++) {
+                if (data.results[i].is_black) {
+                    $scope.policy[i].is_black = "黑名单";
+                }
+                else {
+                    $scope.policy[i].is_black = "白名单";
+                }
+            }
+        });
+    };
+    reload_blackwihit();
+    $scope.router = {
+        "is_black": "1"
+    };
+    $scope.router1 = {
+        "is_black": "1"
+    };
+    $scope.add_mac = function (data) {
+        data.router = router_id;
+        data.content_type = "0";
+        data.enable = "1";
+        $http.post([window.API.ROUTER.NEW_BLACK_WHITES, "?key=", $cookieStore.get("key")].join(""), data).success(function (data) {
+            alert("添加成功");
+            reload_blackwihit();
+        })
+    };
+    $scope.add_domain = function (data) {
+        data.router = router_id;
+        data.content_type = "1";
+        data.enable = "1";
+        $http.post([window.API.ROUTER.NEW_BLACK_WHITES, "?key=", $cookieStore.get("key")].join(""), data).success(function (data) {
+            alert("添加成功");
+            reload_blackwihit();
+        })
+    };
+    $scope.delete = function (id) {
+        var ids = [id];
+        console.log(ids);
+        $http.delete([window.API.ROUTER.REMOVE_BLACK_WHITES, "?key=", $cookieStore.get("key"), "&ids=", ids.join()].join("")).success(function (data) {
+            alert("删除成功");
+            reload_blackwihit();
+        })
+    };
+    $scope.filter = {
+        "content_type": "",
+        "is_black": ""
+    };
+    $scope.blackwhite_filter = function (data) {
+        var filter = angular.copy(data);
+        filter.router = router_id;
+        $http.get([window.API.ROUTER.GET_ROUTER_BLACK_WHITES, "?key=", $cookieStore.get("key"), "&", $.param(filter)].join("")).success(function (data) {
+            $scope.policy = data.results;
+            for (var i = 0; i < data.results.length; i++) {
+                if (data.results[i].is_black) {
+                    $scope.policy[i].is_black = "黑名单";
+                }
+                else {
+                    $scope.policy[i].is_black = "白名单";
+                }
+            }
+        })
+    };
+    //路由基本信息
     $http.get([window.API.ROUTER.GET_ROUTER_SETUP, "?key=", $cookieStore.get("key"), "&router_id=", router_id].join("")).success(function (data) {
-        console.log(data.router);
-        $scope.router_basic = data.router;
+        console.log(data);
+        $scope.routers_all = data;
+        //路由器实时信息
+
+        //默认认证方式
+        if (data.login_type == "手机号认证") {
+            $scope.isActive_phone = 1;
+            $scope.isActive_weixin = 0;
+        }
+        else if (data.login_type == "微信认证") {
+            $scope.isActive_phone = 0;
+            $scope.isActive_weixin = 1;
+        }
+        else if (data.login_type == "手机号认证;微信认证") {
+            $scope.isActive_phone = 1;
+            $scope.isActive_weixin = 1;
+        }
+        else {
+            $scope.isActive_phone = 0;
+            $scope.isActive_weixin = 0;
+        }
+        //默认认证时间
+        if (data.auth_period <= 60) {
+            $scope.identify_time = {
+                "hour": 0,
+                "minute": data.auth_period,
+                "num":data.auth_limit_times
+            };
+        }
+        else {
+            $scope.identify_time = {
+                "hour": parseInt(data.auth_period / 60),
+                "minute": data.auth_period % 60,
+                "num":data.auth_limit_times
+            };
+        }
     });
+    $scope.identify_type = function () {
+        var identify_type = [];
+        if ($scope.isActive_phone == 1) {
+            identify_type.push("手机号认证");
+        }
+        if ($scope.isActive_weixin == 1) {
+            identify_type.push("微信认证");
+        }
+        console.log(identify_type);
+        $http.put([window.API.ROUTER.EDIT_ROUTER_SETUP, "?key=", $cookieStore.get("key"), "&router=", router_id].join(""), {"login_type": identify_type.join(";")});
+    };
+    $scope.identify_submit = function (data) {
+        console.log(data);
+        var times = parseInt(data.hour) * 60 + parseInt(data.minute);
+        console.log(times);
+        $http.put([window.API.ROUTER.EDIT_ROUTER_SETUP, "?key=", $cookieStore.get("key"), "&router=", router_id].join(""), {
+            "auth_period": times,
+            "auth_limit_times": data.num
+        }).success(function (data) {
+            alert(data.msg);
+        })
+    };
+
 }]);
 
 iCloudController.controller("IdentifyConfController", ['$scope', function ($scope) {

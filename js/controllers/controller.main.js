135f4255abb76f0d4f78a20fde8f6350bc9e12e1
357831957 <yangchen@idianjia.cn>
2015-12-08 06:19:35 +0800

    路由器详情修改

--- js/controllers/controller.main.js
+++ js/controllers/controller.main.js
@@ -1,15 +1,15 @@
 /**
  * Created by chen on 2015/10/31.
  */
-iCloudController.controller("MainController", ["$scope", "$http", "$cookieStore", "$rootScope",
-    function ($scope, $http, $cookieStore, $rootScope) {
-        $http.get([window.API.USER.GET_CURRENT_USER_INFO, "?key=", $cookieStore.get("key")].join("")).success(function (data) {
-            $scope.user = {
-                "name": data.name,
-                "identity": data.role_object.name
-            };
+iCloudController.controller("MainController", ["$scope", "$http", "$cookieStore", "$rootScope", function ($scope, $http, $cookieStore, $rootScope) {
+    $http.get([window.API.USER.GET_CURRENT_USER_INFO, "?key=", $cookieStore.get("key")].join("")).success(function (data) {
+        console.log(data);
+        $scope.user = {
+            "name": data.name,
+            "identity": data.role_object.name
+        };
 
-            $cookieStore.put("role", data.role_object.name);
+        $cookieStore.put("role", data.role_object.name);
 
             if (data.role_object.name == "系统管理员") {
                 $scope.auth_nav = false;
@@ -26,6 +26,7 @@ iCloudController.controller("MainController", ["$scope", "$http", "$cookieStore"
                 $scope.marketing_nav = false;
                 $scope.wallet_nav = false;
                 $scope.js_ad_nav = true;
+                $rootScope.update_ssid = false
                 $rootScope.isShow_balckwhite = false;
                 $rootScope.deviceManage = {
                     "addRouterMenu": true,
@@ -52,6 +53,7 @@ iCloudController.controller("MainController", ["$scope", "$http", "$cookieStore"
                 $scope.wallet_nav = true;
                 $scope.info_nav = true;
                 $scope.adsType_nav = 1;
+                $rootScope.update_ssid = true
                 $rootScope.isShow_balckwhite = true;
                 $rootScope.putAdSearch = {
                     "showBusinessLabel": false,
@@ -73,6 +75,7 @@ iCloudController.controller("MainController", ["$scope", "$http", "$cookieStore"
                 $scope.wallet_nav = true;
                 $scope.info_nav = true;
                 $scope.adsType_nav = 2;
+                $rootScope.update_ssid = false
                 $rootScope.isShow_balckwhite = false;
                 $rootScope.putAdSearch = {
                     "showBusinessLabel": true,
